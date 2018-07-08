675698917cc7e6d7c5531552463b917425318f72
johannesharmse <johannesharmse91@gmail.com>
2018-07-08 00:17:31 -0700

    Add payload

--- src/scrape_links.py
+++ src/scrape_links.py
@@ -2,6 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 from lxml import html
 
+
+payload = {
+	"login": "<username>", 
+	"password": "<password>", 
+	"authenticity_token": "<auth_token>"
+}
+
 session_requests = requests.session()
 
 login_url = "https://github.ubc.ca/ubc-mds-2017/jharmse_home"
@@ -11,8 +18,14 @@ tree = html.fromstring(result.text)
 
 # print(result.text)
 
-# authenticity_token = list(set(tree.xpath("//input[@name='csrfmiddlewaretoken']/@value")))[0]
+authenticity_token = list(set(tree.xpath("//input[@name='authenticity_token']/@value")))[0]
+print(authenticity_token)
 
+result = session_requests.post(
+	login_url, 
+	data = payload, 
+	headers = dict(referer=login_url)
+)
 
 # r = requests.get('https://github.ubc.ca/ubc-mds-2017/jharmse_home')
 # data = r.text
